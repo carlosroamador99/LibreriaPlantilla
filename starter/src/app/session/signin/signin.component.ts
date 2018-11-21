260e60914c4fc4c3fee4ad6a3b3559a87c20d451
Carlos Romero <carlosroamador@gmail.com>
2018-11-21 20:36:41 +0100

    plantilla y login

---
+++ starter/src/app/session/signin/signin.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { LoginDto } from 'src/app/Dto/login.dto';
+import { AuthService } from 'src/app/service/auth.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit {
+  email: string;
+  password: string;
+
+  public form: FormGroup;
+  constructor(private authService:AuthService, private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.form = this.fb.group ( {
+      uname: [null , Validators.compose ( [ Validators.required ] )] , password: [null , Validators.compose ( [ Validators.required ] )]
+    } );
+  }
+
+  doLogin() {
+    const loginDto = new LoginDto(this.email, this.password);
+    this.authService.login(loginDto).subscribe(loginResp => {
+      console.log(loginResp);
+      this.authService.setLoginData(loginResp);
+    }, error => {
+  console.log('Error en petición de login');
+  console.log(error);
+    }
+    );
+  }
+  }
